f9457c740ce39f7b6ae92b8372f558cb3a76c602
Angelore <angelore.main@gmail.com>
2022-09-14 00:26:57 +0300

    Fixed typo

--- pack.bat
+++ pack.bat
@@ -1,5 +1,5 @@
-md r6\scrips
-xcopy untrackQuestByRightClick.reds r6\scrips
+md r6\scripts
+xcopy untrackQuestByRightClick.reds r6\scripts
 "D:\Program Files\7-Zip\7z.exe" a untrackQuestByRightClick.zip r6\
 rd r6 /S /Q
 pause
